b7d36f8649e157b40c11e4f28a28dfc45df20f21
pappuparvez1112 <pappuparvezshazz@gmail.com>
2023-08-20 15:46:53 +0600

    [Done] first nextjs project intrigate

---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import RootLayout from "@/components/Layouts/RootLayout";
+import AllNews from "@/components/UI/AllNews";
+import Banner from "@/components/UI/Banner";
+import Head from "next/head";
+
+const HomePage = ({ allNews }) => {
+  console.log(allNews);
+  return (
+    <>
+      <Head>
+        <title>PH-News Portal</title>
+        <meta
+          name="description"
+          content="This is news portal of programming hero made by next-js"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Banner />
+      <AllNews allNews={allNews} />
+    </>
+  );
+};
+export default HomePage;
+
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: {
+      allNews: data,
+    },
+    revalidate: 10,
+  };
+};
